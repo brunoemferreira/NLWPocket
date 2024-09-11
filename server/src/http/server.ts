90da46ee801b9b741363c76e971c7013ab44c851
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-09-11 08:45:20 -0300

    feat: created endpoints

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,61 +1,68 @@
-// biome-ignore lint/style/useImportType: <explanation>
 import {
-    serializerCompiler,
-    validatorCompiler,
-    ZodTypeProvider
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import fastify from "fastify";
 import { createGoal } from "../functions/create-goal";
 import z from "zod";
+import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
+import { createGoalCompletion } from "../functions/create-goal-completion";
 
-
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.post('/goals', {
+app.get("/pending-goals", async () => {
+  const { pendingGoals } = await getWeekPendingGoals();
+
+  return { pendingGoals };
+});
+
+app.post(
+  "/goals",
+  {
     schema: {
-        body: z.object({
-            title: z.string(),
-            desiredWeeklyFrequency: z.number().int().min(1).max(7),
-        }),
-    },
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.post('/goals', async (request) => {
-
-
-
-
-    const createGoalSchema = z.object({
+      body: z.object({
         title: z.string(),
         desiredWeeklyFrequency: z.number().int().min(1).max(7),
-    })
+      }),
+    },
+  },
 
-    const body = createGoalSchema.parse(request.body);
+  async (request) => {
+    const { title, desiredWeeklyFrequency } = request.body;
 
     await createGoal({
-        title: body.title,
-        desiredWeeklyFrequency: body.desiredWeeklyFrequency
-    })
-})
+      title,
+      desiredWeeklyFrequency,
+    });
+  }
+);
 
-app.listen({
+app.post(
+  "/completions",
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  },
+  async (request) => {
+    const { goalId } = request.body;
+
+    await createGoalCompletion({
+      goalId,
+    });
+  }
+);
+
+app
+  .listen({
     port: 3333,
-}).then(() => {
-    console.log('🚀 HTTP Server Running !!!')
-})
+  })
+  .then(() => {
+    console.log("🚀 HTTP Server Running !!!");
+  });
